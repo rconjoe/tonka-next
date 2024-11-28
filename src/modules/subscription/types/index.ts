ebbc8c279eb91091d37fd703057ef4636a4e5e4d
trog <trog@air.local>
2024-11-27 22:53:22 -0800

    wip subscription types

---
+++ src/modules/subscription/types/index.ts
@@ -0,0 +1,11 @@
+export enum SubscriptionStatus {
+  ACTIVE = "active",
+  CANCELED = "canceled",
+  EXPIRED = "expired",
+  FAILED = "failed"
+}
+
+export enum SubscriptionInterval {
+  MONTHLY = "montly",
+  YEARLY = "yearly"
+}
