72aa347789776b007d35fb48c98d4cc8c8526e2d
trog <trog@air.local>
2024-11-27 22:53:01 -0800

    test blog post api route

--- src/api/blog/posts/route.ts
+++ src/api/blog/posts/route.ts
@@ -2,12 +2,16 @@ import type {
   MedusaRequest,
   MedusaResponse
 } from "@medusajs/framework/http"
+import { RemoteLink } from "@medusajs/framework/modules-sdk"
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils"
 import { createPostWorkflow } from "src/workflows/create-post"
 
 export async function POST(
   req: MedusaRequest,
   res: MedusaResponse
 ) {
+
+  const remoteLink: RemoteLink = req.scope.resolve(ContainerRegistrationKeys.REMOTE_LINK)
   const { result: post } = await createPostWorkflow(req.scope)
     .run({
       input: {
