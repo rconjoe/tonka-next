26e1215d551a265faf9b23c604f4a1d2a609cf5a
trog <trog@air.local>
2024-11-30 08:29:56 -0800

    override createSubscriptions
    The createSubscriptions calculates for each subscription the expiration
    and next order dates using the methods created earlier. It creates and
    returns the subscriptions.

--- src/modules/subscription/service.ts
+++ src/modules/subscription/service.ts
@@ -12,6 +12,39 @@ class SubscriptionModuleService extends MedusaService({
   Subscription
 }) {
 
+  // @ts-expect-error
+  async createSubscriptions(
+    data: CreateSubscriptionData | CreateSubscriptionData[]
+  ): Promise<SubscriptionData[]> {
+    const input = Array.isArray(data) ? data : [data]
+
+    const subscriptions = await Promise.all(
+      input.map(async (subscription) => {
+        const subscriptionDate = subscription.subscription_date || new Date()
+        const expirationDate = this.getExpirationDate({
+          subscription_date: subscriptionDate,
+          interval: subscription.interval,
+          period: subscription.period,
+        })
+
+        return await super.createSubscriptions({
+          ...subscription,
+          subscription_date: subscriptionDate,
+          last_order_date: subscriptionDate,
+          next_order_date: this.getNextOrderDate({
+            last_order_date: subscriptionDate,
+            expiration_date: expirationDate,
+            interval: subscription.interval,
+            period: subscription.period,
+          }),
+          expiration_date: expirationDate,
+        })
+      })
+    )
+
+    return subscriptions
+  }
+
   getNextOrderDate({
     last_order_date,
     expiration_date,
