ceaea9f4a4c1de598a4764cd4772539b128e5733
trog <trog@air.local>
2024-11-30 08:20:23 -0800

    getNextOrderDate
    This method accepts a subscription’s last order date, expiration date,
    interval, and period, and uses them to calculate and return the next
    order date.If the next order date, calculated from the last order date,
    exceeds the expiration date, null is returned.

--- src/modules/subscription/service.ts
+++ src/modules/subscription/service.ts
@@ -1,8 +1,36 @@
+import moment from "moment";
 import { MedusaService } from "@medusajs/framework/utils";
 import Subscription from "./models/subscription";
+import {
+  CreateSubscriptionData,
+  SubscriptionData,
+  SubscriptionInterval,
+  SubscriptionStatus
+} from "./types";
 
 class SubscriptionModuleService extends MedusaService({
   Subscription
-}) { }
+}) {
+  getNextOrderDate({
+    last_order_date,
+    expiration_date,
+    interval,
+    period,
+  }: {
+    last_order_date: Date
+    expiration_date: Date
+    interval: SubscriptionInterval
+    period: number
+  }): Date | null {
+    const nextOrderDate = moment(last_order_date)
+      .add(
+        period,
+        interval === SubscriptionInterval.MONTHLY ? "month" : "year"
+      )
+    const expirationMomentDate = moment(expiration_date)
+
+    return nextOrderDate.isAfter(expirationMomentDate) ? null : nextOrderDate.toDate()
+  }
+}
 
 export default SubscriptionModuleService
